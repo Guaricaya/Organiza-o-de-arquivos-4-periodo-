975e896865555956f8746a02e74b9191cfc85a8d
Guaricaya <rafaelbarros240@gmail.com>
2022-10-30 21:58:49 -0300

    indentação e apresentação do código

--- Busca Binaria/Busca binaria.c
+++ Busca Binaria/Busca binaria.c
@@ -23,8 +23,7 @@ int main(int argc, char**argv)
 	long qt;
 	int c, achei;
 	long pos, meio, inicio, fim;
-	if(argc != 2)
-	{
+	if(argc != 2){
 		fprintf(stderr, "USO: %s [CEP]", argv[0]);
 		return 1;
 	}
@@ -38,19 +37,18 @@ int main(int argc, char**argv)
 	qt = pos/sizeof(Endereco);
 	fim = qt - 1;
   
-	while(inicio <= fim && achei<=0)
-	{
+	while(inicio <= fim && achei<=0){
 		c++;
 		meio = (inicio + fim)/2;
 		fseek(f, meio * sizeof(Endereco), SEEK_SET);
 		fread(&e, sizeof(Endereco), 1, f);
 		if(strncmp(argv[1],e.cep,8)==0){			        
-		printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
+			printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n",e.logradouro,e.bairro,e.cidade,e.uf,e.sigla,e.cep);
 			achei++;
 		}else if(strcmp(argv[1],e.cep) < 0){
-		fim = meio - 1;
+			fim = meio - 1;
 		}else{
-		inicio = meio + 1;
+			inicio = meio + 1;
 		}
 		qt = fread(&e,sizeof(Endereco),1,f);		
 	}
